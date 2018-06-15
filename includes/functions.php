9f3f05cc5029b2d8c01539aca1e399c260a5106c
asust <asust@DESKTOP-UU5JP6H.fritz.box>
2018-06-16 01:12:03 +0200

    Adds stuff.

--- includes/functions.php
+++ includes/functions.php
@@ -1,2 +1,10 @@
 <?php
 /* Main functions of sgkey */
+
+function getHardwarePrint() {
+    $output = shell_exec ('hwinfo --short');
+    // cpu + gpu+sound+network+disks+cdrom
+    
+    $output = shell_exec ('sudo hdparm -I /dev/sda' );
+    
+}
